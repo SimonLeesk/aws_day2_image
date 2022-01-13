f99166b3a98c0e3d359c1172d6f3fa1f24c48981
SimonLeesk <93342725+SimonLeesk@users.noreply.github.com>
2022-01-12 16:27:23 -0800

    allow users to upload images

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const express = require("express")
+const multer = require("multer")
+const fs = require('fs')
+
+const database = require('./database')
+
+const upload = multer({ dest: 'images/' })
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.get('/images/:imageName', (req, res) => {
+    const imageName = req.params.imageName
+    const readStream = fs.createReadStream(`images/${imageName}`)
+    readStream.pipe(res)
+  })
+
+app.get("/api/images", async (req, res) => {
+    const images = await database.getImages()
+
+    res.send({images})
+})
+
+app.post("/api/images", upload.single('image'), async (req, res) => {  
+    const imagePath = req.file.path
+    const description = req.body.description
+  
+    // Save this data to a database probably
+    const image = await database.addImage(imagePath, description)
+  
+    res.send({image})
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`listening on port ${port}`))
